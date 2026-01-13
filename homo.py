28c274036bba0b64a7b2dc63563b23377914832c
DanPK <chetverkin.dr@phystech.edu>
2026-01-13 23:59:04 +0300

    angle_detect metod fix

--- homo.py
+++ homo.py
@@ -8,14 +8,18 @@ def mask_preprocessing(mask: np.ndarray):
         m = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)
 
         # Морфологические операции — меньше итераций, адаптивно
-        k = max(3, int(round(min(m.shape[:2]) / 100)))
-        kernel = np.ones((k, k), np.uint8)
-        m = cv2.morphologyEx(m, cv2.MORPH_OPEN, kernel, iterations=1)
+        k = max(20, int(round(min(m.shape[:2]) / 100)))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (k, k))
+        m = cv2.morphologyEx(m, cv2.MORPH_OPEN, kernel, iterations=2)
         m = cv2.morphologyEx(m, cv2.MORPH_CLOSE, kernel, iterations=2)
 
         # Вместо размытия — контурное сглаживание
-        m = cv2.dilate(m, kernel, iterations=1)
-        m = cv2.erode(m, kernel, iterations=1)
+        m = cv2.dilate(m, kernel, iterations=2)
+        m = cv2.erode(m, kernel, iterations=2)
+
+            # Или гауссово размытие + порог
+        m1 = cv2.GaussianBlur(m1, (7, 7), 0)
+        m1 = (m1 > 127).astype(np.uint8)
     return m
 
 
@@ -59,9 +63,7 @@ def has_no_ones(array):
         return not np.any(arr == 1)
     
 
-def create_rect_from_mask_perimeter(mask: np.ndarray, target_ratio = 1):
-    mask2 = cv2.imread(r'template_masks\mask_w585_h585.png', cv2.IMREAD_GRAYSCALE)
-    return  (mask2[:,:,0] > 0).astype(np.uint8)
+
 
 
 def get_max_radius(mask, cx, cy):
@@ -74,13 +76,7 @@ def get_max_radius(mask, cx, cy):
 
 
 def normalize_masks(mask1, mask2):
-    
-    
     # Находим центры масс и площади
-     
-    cv2.imwrite("img/mask01.png", mask1*255)
-    
-    cv2.imwrite("img/mask02.png", mask2*255)
     m1 = cv2.moments(mask1)
     m2 = cv2.moments(mask2)
     
@@ -99,22 +95,19 @@ def normalize_masks(mask1, mask2):
         mask2 = cv2.resize(mask2, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
         m2 = cv2.moments(mask2)
         c2 = (int(m2['m10'] / m2['m00']), int(m2['m01'] / m2['m00']))
-
-
-    
-    cv2.imwrite("img/mask1.png", mask1*255)
-    
-    cv2.imwrite("img/mask2.png", mask2*255)
     # Определяем размер выходного изображения
-    h = max(mask1.shape[0], mask2.shape[0]) + max(c1[1], c2[1])
-    w = max(mask1.shape[1], mask2.shape[1]) + max(c1[0], c2[0])
-
-    target_size = max(w, h)
+    
+    radius = max(get_max_radius(mask1, c1[0], c1[1]), 
+             get_max_radius(mask2, c2[0], c2[1]))
+    
+    target_size = max(mask1.shape[0], mask1.shape[1], mask2.shape[0], mask2.shape[1], 2*radius) 
     if target_size % 2 != 0: target_size += 1
     
     # Центрируем обе маски
     canvas1 = np.zeros((target_size, target_size), dtype=np.uint8)
     canvas2 = np.zeros((target_size, target_size), dtype=np.uint8)
+
+    print(canvas1.shape, mask1.shape)
     
     y1, x1 = target_size // 2 - c1[1], target_size // 2 - c1[0]
     y2, x2 = target_size // 2 - c2[1], target_size // 2 - c2[0]
@@ -168,13 +161,14 @@ def calculate_angle(warped_mask: np.ndarray, template_mask: np.ndarray) -> float
     return float(angle)
 
 
+def find_template(mask: np.ndarray, target_ratio = 1):
+    mask2 = cv2.imread(r'template_masks\mask_w820_h957.png', cv2.IMREAD_GRAYSCALE)
+    return  (mask2[:,:,0] > 0).astype(np.uint8)
+
+
 def detect_sheet_angle_no_homography(warped_mask: np.ndarray) -> float:
 
-
-
-    warped_mask = mask_preprocessing(warped_mask)
-
-    template_mask = create_rect_from_mask_perimeter(warped_mask)
+    template_mask = find_template(warped_mask)
 
     x, y, w_c, h_c = cv2.boundingRect(cv2.findNonZero(warped_mask))
     warped_mask_cropped = warped_mask[y:y+h_c, x:x+w_c]
@@ -183,5 +177,7 @@ def detect_sheet_angle_no_homography(warped_mask: np.ndarray) -> float:
     template_mask_cropped = template_mask[y2:y2+h_c2, x2:x2+w_c2]
 
     norm_warped_mask, norm_template_mask = normalize_masks(warped_mask_cropped, template_mask_cropped)
+    #norm_warped_mask = mask_preprocessing(norm_warped_mask)
+    #norm_warped_mask, norm_template_mask = normalize_masks(norm_warped_mask, norm_template_mask)
 
     return calculate_angle(norm_warped_mask, norm_template_mask)
